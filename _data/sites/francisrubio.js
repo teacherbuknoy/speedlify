799661a4ed276edee32087854e0e266795ae4641
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2023-01-29 00:54:58 +0800

    Add my sites for benchmarking

---
+++ _data/sites/francisrubio.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "Francis Rubio", // optional, falls back to object key
+	description: "The sites that get tested",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://antaresph.dev/",
+		"https://francisrubio.antaresph.dev/"
+	]
+};
